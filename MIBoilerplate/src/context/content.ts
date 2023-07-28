9f2f3d6477a11e7ca0640b3dc15d03dbcb17809a
Premshankar Shah <premshankar.shah@mindinventory.com>
2023-07-28 14:59:59 +0530

    refactor(contents): :zap: refactor content

--- MIBoilerplate/src/context/content.ts
+++ MIBoilerplate/src/context/content.ts
@@ -1,40 +1,6 @@
 import { TranslateOptions } from 'i18n-js';
 
-import i18n from '../i18n';
-import type en from '../i18n/locales/en.json';
-
-export type DefaultContentType = keyof typeof en;
-
-export enum ContentLanguage {
-  English = 'en',
-  Hindi = 'hi',
-}
-
-export type TxKeyPath = RecursiveKeyOf<typeof en>;
-
-// via: https://stackoverflow.com/a/65333050
-type RecursiveKeyOf<TObj extends object> = {
-  [TKey in keyof TObj & (string | number)]: RecursiveKeyOfHandleValue<
-    TObj[TKey],
-    `${TKey}`
-  >;
-}[keyof TObj & (string | number)];
-
-type RecursiveKeyOfInner<TObj extends object> = {
-  [TKey in keyof TObj & (string | number)]: RecursiveKeyOfHandleValue<
-    TObj[TKey],
-    `['${TKey}']` | `.${TKey}`
-  >;
-}[keyof TObj & (string | number)];
-
-type RecursiveKeyOfHandleValue<
-  TValue,
-  Text extends string
-> = TValue extends any[]
-  ? Text
-  : TValue extends object
-  ? Text | `${Text}${RecursiveKeyOfInner<TValue>}`
-  : Text;
+import i18n, { TxKeyPath } from '../i18n';
 
 /**
  * Translates text.
--- MIBoilerplate/src/i18n/index.ts
+++ MIBoilerplate/src/i18n/index.ts
@@ -23,3 +23,34 @@ i18n.translations = {
 };
 
 export default i18n;
+
+export enum ContentLanguage {
+  English = 'en',
+  Hindi = 'hi',
+}
+
+export type TxKeyPath = RecursiveKeyOf<typeof en>;
+
+// via: https://stackoverflow.com/a/65333050
+type RecursiveKeyOf<TObj extends object> = {
+  [TKey in keyof TObj & (string | number)]: RecursiveKeyOfHandleValue<
+    TObj[TKey],
+    `${TKey}`
+  >;
+}[keyof TObj & (string | number)];
+
+type RecursiveKeyOfInner<TObj extends object> = {
+  [TKey in keyof TObj & (string | number)]: RecursiveKeyOfHandleValue<
+    TObj[TKey],
+    `['${TKey}']` | `.${TKey}`
+  >;
+}[keyof TObj & (string | number)];
+
+type RecursiveKeyOfHandleValue<
+  TValue,
+  Text extends string
+> = TValue extends any[]
+  ? Text
+  : TValue extends object
+  ? Text | `${Text}${RecursiveKeyOfInner<TValue>}`
+  : Text;
