75c1b47801dfd0a0c47836df2adc04262c2ae710
Ankit <ankit.prajapati@mindinventory.com>
2024-05-29 12:40:05 +0530

    feat(react native): some changes in script file

--- script.js
+++ script.js
@@ -1,59 +1,59 @@
 #!/usr/bin/env node
 
-console.log("Welcome to Mindinventory React native boilerplate");
+console.log("Welcome to Mindinventory React native boilerplate")
 
-const { execSync } = require("child_process");
+const { execSync } = require("child_process")
 
 const installDependencies = () => {
-  console.log("\n\n");
+  console.log("\n\n")
 
   console.log(
     "@mindinventory/react-native-boilerplate initialized with success! 🚀\n"
-  );
+  )
 
-  console.log("Installing dependencies... 🛠️\n");
-  execSync(`yarn`, { stdio: "inherit" });
-  console.log("Dependencies installed successfully. 🚀\n");
+  console.log("Installing dependencies... 🛠️\n")
+  execSync(`yarn`, { stdio: "inherit" })
+  console.log("Dependencies installed successfully. 🚀\n")
 
-  console.log("bundle Installing 🛠️\n");
-  execSync(`bundle`, { stdio: "inherit" });
-  console.log("bundle installed successfully.🚀\n");
+  console.log("bundle Installing 🛠️\n")
+  execSync(`bundle install`, { stdio: "inherit" })
+  console.log("bundle installed successfully.🚀\n")
 
-  console.log("pod-install Installing 🛠️\n");
-  execSync(`npx pod-install`, { stdio: "inherit" });
-  console.log("pod-install installed successfully.🚀\n");
-};
+  console.log("pod-install Installing 🛠️\n")
+  execSync(`npx pod-install`, { stdio: "inherit" })
+  console.log("pod-install installed successfully.🚀\n")
+}
 
 const renameNpmignoreToGitignore = () => {
-  execSync("mv .npmignore .gitignore", { stdio: "inherit" });
-};
+  execSync("mv .npmignore .gitignore", { stdio: "inherit" })
+}
 
 const main = () => {
-  execSync("git init", { stdio: "inherit" });
-  installDependencies();
-  renameNpmignoreToGitignore();
-};
+  execSync("git init", { stdio: "inherit" })
+  installDependencies()
+  renameNpmignoreToGitignore()
+}
 
 new Promise((resolve) => {
-  main();
-  resolve();
+  main()
+  resolve()
 })
   .then(() => {
     console.log(
       "- 🎉  Congrats! Your project is ready with @mindinventory/react-native-boilerplate! 🎉\n"
-    );
+    )
 
     console.log(
       "- 📚 If you need to read more about this boilerplate : https://github.com/Mindinventory/react-native-boilerplate/blob/master/README.md\n"
-    );
+    )
     console.log(
       "- 🤕 If you have some troubles : https://github.com/Mindinventory/react-native-boilerplate/issues\n"
-    );
+    )
     console.log(
       "- ⭐ If you love this boilerplate, give us a star, you will be a ray of sunshine in our lives :) https://github.com/Mindinventory/react-native-boilerplate\n"
-    );
+    )
   })
   .catch((error) => {
-    console.error(`🚨 An error occurred with post init script: ${error}`);
-    throw new Error();
-  });
+    console.error(`🚨 An error occurred with post init script: ${error}`)
+    throw new Error()
+  })
