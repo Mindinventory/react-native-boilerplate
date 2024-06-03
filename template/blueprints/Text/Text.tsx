3d0a2ccb9d9b8a28ada0b4ac51413b1a197b9f3d
Ankit Prajapati <ankit.prajapati@mindinventory.com>
2024-06-03 11:57:16 +0530

    feat(added fonts): added custom fonts

--- template/blueprints/Text/Text.tsx
+++ template/blueprints/Text/Text.tsx
@@ -10,9 +10,9 @@ import {
 import { useColor } from '@src/context';
 import { scaledSize } from '@src/utils';
 
-// export enum Fonts {
-//   Poppins = 'Poppins',
-// }
+export enum Fonts {
+  Poppins = 'Poppins',
+}
 
 const BASE_TEXT: TextStyle = {
   fontSize: scaledSize(7),
@@ -23,68 +23,68 @@ export const presets = {
   font400: {
     ...BASE_TEXT,
     //add your font normal for weight 400
-    // fontFamily: Fonts.Poppins,
+    fontFamily: Fonts.Poppins,
   } as TextStyle,
   font500: {
     ...BASE_TEXT,
     //add your font medium for weight 500
-    // fontFamily: Fonts.Poppins,
+    fontFamily: Fonts.Poppins,
   } as TextStyle,
   font600: {
     ...BASE_TEXT,
     //add your font semi-bold for weight 600
-    // fontFamily: Fonts.Poppins,
+    fontFamily: Fonts.Poppins,
   } as TextStyle,
   font700: {
     ...BASE_TEXT,
     //add your font bold for weight 700
-    // fontFamily: Fonts.Poppins,
+    fontFamily: Fonts.Poppins,
   } as TextStyle,
   h1: {
     ...BASE_TEXT,
-    // fontFamily: Fonts.Poppins,
+    fontFamily: Fonts.Poppins,
     fontSize: scaledSize(24),
     fontWeight: '700',
   } as TextStyle,
   h2: {
     ...BASE_TEXT,
-    // fontFamily: Fonts.Poppins,
+    fontFamily: Fonts.Poppins,
     fontSize: scaledSize(21),
     fontWeight: '700',
   } as TextStyle,
   h3: {
     ...BASE_TEXT,
-    // fontFamily: Fonts.Poppins,
+    fontFamily: Fonts.Poppins,
     fontSize: scaledSize(18),
     fontWeight: '500',
   } as TextStyle,
   h4: {
     ...BASE_TEXT,
-    // fontFamily: Fonts.Poppins,
+    fontFamily: Fonts.Poppins,
     fontSize: scaledSize(15),
     fontWeight: '500',
   } as TextStyle,
   h5: {
     ...BASE_TEXT,
-    // fontFamily: Fonts.Poppins,
+    fontFamily: Fonts.Poppins,
     fontSize: scaledSize(12),
     fontWeight: '400',
   } as TextStyle,
   h6: {
     ...BASE_TEXT,
-    // fontFamily: Fonts.Poppins,
+    fontFamily: Fonts.Poppins,
     fontSize: scaledSize(9),
     fontWeight: '400',
   } as TextStyle,
   small: {
     ...BASE_TEXT,
-    // fontFamily: Fonts.Poppins,
+    fontFamily: Fonts.Poppins,
     fontSize: scaledSize(6),
     fontWeight: '300',
   } as TextStyle,
   title: {
     ...BASE_TEXT,
-    // fontFamily: Fonts.Poppins,
+    fontFamily: Fonts.Poppins,
     fontSize: scaledSize(13),
     fontWeight: '700',
   } as TextStyle,
---
+++ template/react-native.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  assets: ['./src/assets/fonts'],
+  project: {
+    android: {},
+    ios: {},
+  },
+};
