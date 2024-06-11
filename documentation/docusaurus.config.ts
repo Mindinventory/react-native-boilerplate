949c2802a07e1a43267bf51428a735037668c5d7
Ankit Prajapati <ankit.prajapati@mindinventory.com>
2024-06-11 17:34:22 +0530

    feat(config): removed blog route from config

--- documentation/docusaurus.config.ts
+++ documentation/docusaurus.config.ts
@@ -40,13 +40,7 @@ const config: Config = {
           editUrl:
             'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
+     
         theme: {
           customCss: './src/css/custom.css',
         },
@@ -110,10 +104,7 @@ const config: Config = {
         {
           title: 'More',
           items: [
-            {
-              label: 'Blog',
-              to: '/blog',
-            },
+           
             {
               label: 'GitHub',
               href: 'https://github.com/facebook/docusaurus',
