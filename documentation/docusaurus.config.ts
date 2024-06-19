4b64e5dc3824292f98317a6cf4f610db1a28894f
Ankit Prajapati <ankit.prajapati@mindinventory.com>
2024-06-19 09:57:37 +0530

    feat(seo changes): added meta data and make some changes

--- documentation/docusaurus.config.ts
+++ documentation/docusaurus.config.ts
@@ -6,7 +6,7 @@ import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
   title: 'React Native Boilerplate',
-  tagline: `Boilerplate code provides a pre-made foundation for your project, including essential packages, reusable components, and a structured architecture. This jumpstarts development by saving you time on initial setup`,
+  tagline: `Boilerplate code provides a pre-made foundation for your project, including essential packages, reusable components, and a structured architecture. Build modern applications effortlessly with our streamlined React Native boilerplate, designed for simplicity and scalability. Join our active community to collaborate, share ideas, and accelerate your app development journey.`,
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
@@ -98,7 +98,7 @@ const config: Config = {
             },
             {
               label: 'linkedin',
-              href: 'https://www.linkedin.com/company/mindinventory/mycompany/verification/',
+              href: 'https://www.linkedin.com/company/mindinventory',
             },
           ],
         },
@@ -134,7 +134,10 @@ const config: Config = {
     },
     colorMode:{
       disableSwitch: true
-    }
+    },
+    metadata: [
+      {name: 'Best React Native Boilerplate By MindInventory', content: 'Build high-performance apps with our React Native template. Select between JavaScript and TypeScript for a seamless development experience.'},      
+    ],
   } satisfies Preset.ThemeConfig,
   
   trailingSlash: true,
--- documentation/src/pages/index.tsx
+++ documentation/src/pages/index.tsx
@@ -2,7 +2,6 @@ import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import Heading from '@theme/Heading';
 
 import styles from './index.module.css';
@@ -30,14 +29,13 @@ function HomepageHeader() {
 
 export default function Home(): JSX.Element {
   const { siteConfig } = useDocusaurusContext();
+  console.log('siteConfig :::: ', siteConfig);
+
   return (
     <Layout
       title={`${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+    >
       <HomepageHeader />
-      {/* <main>
-        <HomepageFeatures />
-      </main> */}
     </Layout>
   );
 }
