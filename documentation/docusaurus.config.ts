b958ee24992de481cc030115e84b5d5d7ab6cd05
Ankit Prajapati <ankit.prajapati@mindinventory.com>
2024-06-19 14:29:11 +0530

    feat(home page): implemented tailwind css

--- documentation/docusaurus.config.ts
+++ documentation/docusaurus.config.ts
@@ -141,6 +141,19 @@ const config: Config = {
   } satisfies Preset.ThemeConfig,
   
   trailingSlash: true,
+  plugins: [
+    async function myPlugin(context, options) {
+      return {
+        name: "docusaurus-tailwindcss",
+        configurePostCss(postcssOptions) {
+          // Appends TailwindCSS and AutoPrefixer.
+          postcssOptions.plugins.push(require("tailwindcss"));
+          postcssOptions.plugins.push(require("autoprefixer"));
+          return postcssOptions;
+        },
+      };
+    },
+  ]
 };
 
 export default config;
