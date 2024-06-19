0432935a4c5dc6f9827693cfebb8d9b3ebcab3f2
Ankit Prajapati <ankit.prajapati@mindinventory.com>
2024-06-19 09:59:43 +0530

    feat(seo changes): removed title props from home page

--- documentation/src/pages/index.tsx
+++ documentation/src/pages/index.tsx
@@ -31,9 +31,7 @@ export default function Home(): JSX.Element {
   const { siteConfig } = useDocusaurusContext();
 
   return (
-    <Layout
-      title={`${siteConfig.title}`}
-    >
+    <Layout>
       <HomepageHeader />
     </Layout>
   );
