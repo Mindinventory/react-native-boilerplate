dea05edf2ba32c7b2f90a6156a30a2a05f1d437f
Ankit Prajapati <ankit.prajapati@mindinventory.com>
2024-07-02 10:55:52 +0530

    feat(config): removed edit this page link

--- documentation/docusaurus.config.ts
+++ documentation/docusaurus.config.ts
@@ -38,9 +38,7 @@ const config: Config = {
         docs: {
           sidebarPath: './sidebars.ts',
           // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          // Remove this to remove the "edit this page" links.         
         },
      
         theme: {
