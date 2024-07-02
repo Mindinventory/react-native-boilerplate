2b321a4d2b0e5266e5563ad3f54d36b63de19224
ankitpmi <83645232+ankitpmi@users.noreply.github.com>
2024-07-02 11:17:24 +0530

    fix(refactoring): removed unused code (#26)

--- documentation/src/components/HomepageFeatures/index.tsx
+++ documentation/src/components/HomepageFeatures/index.tsx
@@ -1,5 +1,4 @@
 import clsx from 'clsx';
-import Heading from '@theme/Heading';
 import styles from './styles.module.css';
 
 type FeatureItem = {
--- documentation/src/pages/index.tsx
+++ documentation/src/pages/index.tsx
@@ -1,36 +1,8 @@
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
-import Heading from '@theme/Heading';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import HomepageHeader from '@site/src/components/HomepageHeader'
 
-import styles from './index.module.css';
-
-// function HomepageHeader() {
-//   const { siteConfig } = useDocusaurusContext();
-//   return (
-//     <header className={clsx('hero--primary', styles.heroBanner)}>
-//       <div className={clsx('container', styles.container_layout)}>
-//         <Heading as="h1" className={clsx('hero__title', styles.header_text)}>
-//           React Native Boilerplate
-//         </Heading>
-//         <p className={clsx('hero__subtitle', styles.subtitle)}>{siteConfig.tagline}</p>
-//         <div className={styles.buttons}>
-//           <Link
-//             className="button button--primary button--lg"
-//             to="/docs/intro">
-//             Get started
-//           </Link>
-//         </div>
-//       </div>
-//     </header>
-//   );
-// }
-
 export default function Home(): JSX.Element {
-
   return (
     <Layout>
       <HomepageHeader />
