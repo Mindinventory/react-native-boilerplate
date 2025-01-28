53ad213e7e5173bf6cb0415db2c749bee4b99168
Chirag Rami <chirag.rami@mindinventory.com>
2025-01-28 15:13:53 +0530

    refactor: Add support for button variants and loader in AnimatedTouchableOpacity
    - Added support for button variants (standard and outlined) in AnimatedTouchableOpacity component
    - Implemented loader functionality to show loading state in the button
    - Updated the styles and behavior of the button based on the variant and loading state
    - Refactored the code to improve readability and maintainability

--- templates/CliTemplate/src/utils/color.ts
+++ templates/CliTemplate/src/utils/color.ts
@@ -6,36 +6,42 @@ export const color = {
     primaryColor: '#0a84ff', // bright blue
     secondaryColor: '#dcdcdc', // dark grey
     textColor: '#f8f9fa', // off-white
+    buttonTextColor: '#FFFFFF',
   },
   light: {
     backgroundColor: '#f8f9fa', // grey
     primaryColor: '#000080', // blue
     secondaryColor: '#6c757d', // off-white
     textColor: '#343a40', // dark grey
+    buttonTextColor: '#FFFFFF',
   },
   theme1: {
     backgroundColor: '#f8f8f8', // light pink
     primaryColor: '#ff5a5f', // red
     secondaryColor: '#f2c9c9', // off-white
     textColor: '#424242', // dark grey
+    buttonTextColor: '#FFFFFF',
   },
   theme2: {
     backgroundColor: '#e5e5e5', // wheat
     primaryColor: '#000080', // navy blue
     secondaryColor: '#f5deb3', // light grey
     textColor: '#333333', // dark grey
+    buttonTextColor: '#FFFFFF',
   },
   theme3: {
     backgroundColor: '#f0f0f0', // light yellow
     primaryColor: '#800000', // maroon
     secondaryColor: '#ffffe0', // light grey
     textColor: '#2f4f4f', // dark slate grey
+    buttonTextColor: '#FFFFFF',
   },
   theme4: {
     backgroundColor: '#f5f5f5', // misty rose
     primaryColor: '#663399', // rebecca purple
     secondaryColor: '#ffe4e1', // light grey
     textColor: '#333333', // dark grey
+    buttonTextColor: '#FFFFFF',
   },
 };
 
